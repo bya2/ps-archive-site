5f378b6b64aef9a945030549ff77bf8db43cca6b
bya2 <byaa1972@gmail.com>
2021-10-17 23:56:59 +0900

    [모듈/작성] PROD:connect-redis / 세션 데이터를 DB에 저장, 세션을 통해 로그인 및 로그아웃 구현

--- app.js
+++ app.js
@@ -9,22 +9,34 @@ const express = require('express');
 const cookie_parser = require('cookie-parser');
 const session = require('express-session');
 
-const HTTP_PORT = process.env.HTTP_PORT || 3000;
+const redis = require('redis');
+
+const HTTP_PORT = process.env.HTTP_PORT || 80;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 (() => {
   const app = express();
+
+  const obj_redis_client_options = {
+    host: '127.0.0.1',
+    port: 6379,
+  }
+  let redis_client = redis.createClient(obj_redis_client_options);
+  let redis_store = require('connect-redis')(session);
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookie_parser());
   app.use(session({
+    key: 'login_data',
+    secret: process.env.COOKIE_SECRET || '',
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET || '',
+    store: new redis_store({ client: redis_client, logErrors: true }),
     cookie: {
       path: '/',
       httpOnly: true,
-      maxAge: 10000,
+      maxAge: 1000 * 60 * 60,
       secure: false,
     },
   }));
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,30 +3,146 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const req_cookie = req.cookies;
-  console.log(req_cookie);
+const fn__mw__err_handler = (req, res) => {
+  const obj__err_info = {
+    dir: __filename,
+    instance: 'router',
+    method: '',
+    path: '',
+    handler: 'callback',
+  };
 
-  console.log(req.query);
-  console.log(req.body);
+  console.error(`
+    An error occurred at the following location:\n
+    ${obj__err_info}\n
+    Error:\n
+    ${err}
+  `);
+}
 
-  res.sendFile('index.html', {
-    root: path.join(process.cwd(), 'public'),
-    
-  });
+router.get('/', (req, res) => {
+  'use strict';
+  try {
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      res.status(200)
+         .sendFile('index.html', { root: path.join(process.cwd(), 'public') });
+    } else {
+      res.status(200)
+         .sendFile('index.html', { root: path.join(process.cwd(), 'public') });
+    }
+
+  } catch (err) {
+    const obj__err_info = {
+      dir: __filename,
+      instance: 'router',
+      method: 'get',
+      path: '/',
+      handler: 'callback',
+    };
+
+    console.error(`
+      An error occurred at the following location:\n
+      ${obj__err_info}\n
+      Error:\n
+      ${err}
+    `);
+  }
 });
 
-router.post('/api/auth/logout', (req, res, next) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.redirect(301, '/');
+router.get('/login', (req, res) => {
+  'use strict';
+  try {
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      res.status(200)
+         .redirect(301, '/');
+    } else {
+      res.status(200)
+         .sendFile('login.html', { root: path.join(process.cwd(), 'public') })
+    }
+  } catch (err) {
+    const obj__err_info = {
+      dir: __filename,
+      instance: 'router',
+      method: 'get',
+      path: '/login',
+      handler: 'callback',
+    };
+
+    console.error(`
+      An error occurred at the following location:\n
+      ${obj__err_info}\n
+      Error:\n
+      ${err}
+    `);
+  }
 })
 
-router.post('/api/auth/login', (req, res, next) => {
-  res
-  .status(201)
-  .cookie('ok2', 1)
-  .redirect(301, '/');
+router.post('/api/auth/login_process', (req, res) => {
+  'use strict';
+  try {
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+
+    } else {
+      req.session.user = req.body.n_id;
+    }
+
+    res.status(200)
+       .redirect(301, '/');
+  } catch (err) {
+    const obj__err_info = {
+      dir: __filename,
+      instance: 'router',
+      method: 'get',
+      path: '/api/auth/login_process',
+      handler: 'callback',
+    };
+
+    console.error(`
+      An error occurred at the following location:\n
+      ${obj__err_info}\n
+      Error:\n
+      ${err}
+    `);
+  }
+});
+
+router.post('/api/auth/logout_process', (req, res) => {
+  'use strict';
+  try {
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      req.session.destroy(_err => {
+        if (_err) throw _err;
+        res.status(201)
+           .redirect(301, '/');
+      });
+    }
+
+    res.status(200)
+       .redirect(301, '/');
+  } catch (err) {
+    const obj__err_info = {
+      dir: __filename,
+      instance: 'router',
+      method: 'get',
+      path: '/api/auth/logout_process',
+      handler: 'callback',
+    };
+
+    console.error(`
+      An error occurred at the following location:\n
+      ${obj__err_info}\n
+      Error:\n
+      ${err}
+    `);
+  }
 });
 
 module.exports = router;
