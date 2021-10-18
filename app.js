7691c8b3b1d90677f265fb8b6ed55809d6113767
bya2 <byaa1972@gmail.com>
2021-10-18 21:38:58 +0900

    [작성] 라우트 에러 처리 관련 작성(비동기 에러 처리)

--- app.js
+++ app.js
@@ -11,15 +11,22 @@ const session = require('express-session');
 
 const redis = require('redis');
 
+const { AssertionError } = require('assert');
+const { RedisError } = redis
+
+const { ctrls__errors, ctrls__routes } = require('./src/controllers');
+
+const HOST = process.env.HOST || '127.0.0.1';
 const HTTP_PORT = process.env.HTTP_PORT || 80;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 (() => {
   const app = express();
+  const router = express.Router();
 
   const obj_redis_client_options = {
-    host: '127.0.0.1',
-    port: 6379,
+    host: HOST,
+    port: REDIS_PORT,
   }
   let redis_client = redis.createClient(obj_redis_client_options);
   let redis_store = require('connect-redis')(session);
@@ -41,7 +48,83 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
     },
   }));
 
-  app.use('/', require('./src/routes'));
+  router.get('/', fn__wrap_async(async (req, res, next) => {
+    'use strict';
+    const bool__is_login = req.session.user ? true : false;
 
-  const server = app.listen(HTTP_PORT, () => console.log(`LISTENING:\nhttp://127.0.0.1:${HTTP_PORT}`));
+    console.log(req.cookie);
+    console.log(req.session);
+
+    if (bool__is_login) {
+      res.send(`Hi, ${req.session.user}`);
+    } else {
+      res.status(200)
+         .sendFile('index.html', { root: path.join(process.cwd(), 'public')});
+    }
+  }));
+
+  router.get('/signup', (req, res, next) => {
+    'use strict';
+  });
+
+  router.get('/login', fn__wrap_async(async (req, res, next) => {
+    'use strict';
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      res.redirect(303, '/');
+    } else {
+      res.status(200)
+         .sendFile('login.html', { root: path.join(process.cwd(), 'public')});
+    }
+  }));
+
+  router.post('/api/auth/login', fn__wrap_async(async (req, res, next) => {
+    'use strict';
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      res.redirect(303, '/');
+    } else {
+      req.session.user = req.body.n_id;
+      req.session.password = req.body.n_pw;
+      res.redirect(301, '/');
+    }
+  }));
+
+  router.post('/api/auth/logout', fn__wrap_async(async (req, res, next) => {
+    'use strict';
+    const bool__is_login = req.session.user ? true : false;
+
+    if (bool__is_login) {
+      req.session.destroy();
+      res.redirect(301, '/');
+    } else {
+      res.redirect(303, '/');
+    }
+  }))
+
+  app.use('/', router);
+
+  app.use((req, res, next) => {
+    res.status(404)
+       .send('404. Not found.');
+  })
+
+  app.use((err, req, res, next) => {
+    res.status(500)
+       .json({
+         message: err.stack
+       });
+
+    next(err);
+  });
+
+  const server = app.listen(HTTP_PORT, () => console.log(`LISTENING:\nhttp://${HOST}:${HTTP_PORT}`));
+
+  function fn__wrap_async (_fn) {
+    return (req, res, next) => {
+      _fn(req, res, next).catch(next);
+    }
+  }
 })();
